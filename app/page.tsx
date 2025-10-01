13338b494eedf64ce45cf8ccc3da1461b34c84bc
Modi Meet <meetmodi8080@gmail.com>
2025-10-01 22:45:19 +0530

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import Hero from "../components/hero/Hero";
+import React from 'react';
+import { Suspense } from 'react';
+import Hero from '@/components/hero/Hero';
 
 export default function Home() {
   return (
-    <>
+    <Suspense fallback={<div className="w-full h-screen bg-black" />}>
       <Hero />
-    </>
+    </Suspense>
   );
 }
